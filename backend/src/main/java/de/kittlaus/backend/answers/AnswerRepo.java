37462275cd56362399bba5302b1b6682bfbc5a01
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-11-24 20:20:54 +0100

    Endpoints open

--- backend/src/main/java/de/kittlaus/backend/answers/AnswerRepo.java
+++ backend/src/main/java/de/kittlaus/backend/answers/AnswerRepo.java
@@ -3,5 +3,8 @@ package de.kittlaus.backend.answers;
 import de.kittlaus.backend.models.answers.ValidatedAnswer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface AnswerRepo  extends MongoRepository<ValidatedAnswer,String> {
+    List<ValidatedAnswer> findAllByUserIdAndExam(String username,boolean exam);
 }
